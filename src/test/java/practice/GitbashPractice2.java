8fc6d295575b3aabb622842afc4ff946aae9e5a7
Dunu27 <dunu27@gmail.com>
2023-03-10 14:37:56 +0530

    Commit test

--- src/test/java/practice/GitbashPractice2.java
+++ src/test/java/practice/GitbashPractice2.java
@@ -2,13 +2,13 @@ package practice;
 
 import org.testng.annotations.Test;
 
-public class GitbashPratice {
-@Test
+public class GitbashPractice2 {
+	@Test
 	public  void main() {
-System.out.println("Test1--class1");
+System.out.println("Test1--class2");
 	}
 @Test
 public  void main2() {
-System.out.println("Test2--class1");
+System.out.println("Test2--class2");
 }
 }
