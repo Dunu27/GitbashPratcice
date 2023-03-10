446d2ab7052907317a18101db708bd3ca4625ed7
Dunu27 <dunu27@gmail.com>
2023-03-10 17:21:38 +0530

    FetchRuntime

--- src/test/java/practice/GitbashPratice.java
+++ src/test/java/practice/GitbashPratice.java
@@ -6,9 +6,15 @@ public class GitbashPratice {
 @Test
 	public  void main() {
 System.out.println("Test1--class1");
+String url=System.getProperty("url");
+String browser=System.getProperty("browser");
+System.out.println(url+" "+browser);
 	}
 @Test
 public  void main2() {
 System.out.println("Test2--class1");
+String url=System.getProperty("url");
+String browser=System.getProperty("browser");
+System.out.println(url+" "+browser);
 }
 }
